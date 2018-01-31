7c4984089f5079ffc2f57c1676993d86496a9303
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-31 19:42:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * use lstat-dir ^2.1.0 to support Buffer and URL paths
    * update @shinnn/eslint-config and follow its coding rules, for example tab indent

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const lstatDir = require('lstat-dir');
 
 module.exports = async function listEmptyFiles(...args) {
-  const paths = new Set();
+	const paths = new Set();
 
-  for (const [path, stat] of await lstatDir(...args)) {
-    if (stat.isFile() && stat.size === 0) {
-      paths.add(path);
-    }
-  }
+	for (const [path, stat] of await lstatDir(...args)) {
+		if (stat.isFile() && stat.size === 0) {
+			paths.add(path);
+		}
+	}
 
-  return paths;
+	return paths;
 };
--- test.js
+++ test.js
@@ -8,56 +8,64 @@ const test = require('tape');
 const writeFileAtomically = require('write-file-atomically');
 
 test('listEmptyFiles()', async t => {
-  t.plan(7);
+	await Promise.all(['10', '2'].map(filename => writeFileAtomically(join(__dirname, filename), '')));
 
-  await Promise.all(['10', '2'].map(filename => writeFileAtomically(join(__dirname, filename), '')));
+	const files = await listEmptyFiles(__dirname, {numeric: true});
+	t.ok(files instanceof Set, 'should retrieve a Set instance.');
+	t.deepEqual([...files], [
+		'2',
+		'10'
+	].map(path => join(__dirname, path)), 'should list empty files in a directory.');
 
-  listEmptyFiles(__dirname, {numeric: true}).then(files => {
-    t.ok(files instanceof Set, 'should be fulfilled with a Set instance.');
+	await rmfr('{10,2}', {
+		glob: {
+			cwd: __dirname
+		}
+	});
 
-    t.deepEqual([...files], [
-      '2',
-      '10'
-    ].map(path => join(__dirname, path)), 'should list empty files in a directory.');
-  }).then(() => rmfr('{10,2}', {
-    glob: {
-      cwd: __dirname
-    }
-  })).catch(t.fail);
+	t.equal(
+		(await listEmptyFiles(Buffer.from(join(__dirname, 'node_modules')))).size,
+		0,
+		'should retrieve an empty Set if the directory has no empty files.'
+	);
 
-  listEmptyFiles(join(__dirname, 'node_modules')).then(files => {
-    t.strictEqual(
-      files.size,
-      0,
-      'should be fulfilled with an empty Set if the directory contains no empty files.'
-    );
-  }).catch(t.fail);
+	const fail = t.fail.bind(t, 'Unexpectedly succeeded.');
 
-  listEmptyFiles('not-found').catch(err => {
-    t.strictEqual(err.code, 'ENOENT', 'should fail when it cannot find the directory.');
-  });
+	try {
+		await listEmptyFiles('not-found');
+		fail();
+	} catch ({code}) {
+		t.equal(code, 'ENOENT', 'should fail when it cannot find the directory.');
+	}
 
-  listEmptyFiles([0, 1]).catch(err => {
-    t.strictEqual(
-      err.toString(),
-      'TypeError: Expected a directory path (string), but got [ 0, 1 ] (array).',
-      'should fail when it takes a non-string argument.'
-    );
-  });
+	try {
+		await listEmptyFiles([0, 1]);
+		fail();
+	} catch ({name}) {
+		t.equal(name, 'TypeError', 'should fail when it takes a non-string argument.');
+	}
 
-  listEmptyFiles().catch(err => {
-    t.strictEqual(
-      err.toString(),
-      'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got no arguments.',
-      'should fail when it takes no arguments.'
-    );
-  });
+	try {
+		await listEmptyFiles();
+		fail();
+	} catch (err) {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected 1 or 2 arguments (path: <string|Buffer|URL>[, options: <Object>]), but got no arguments.',
+			'should fail when it takes no arguments.'
+		);
+	}
 
-  listEmptyFiles('a', {}, 'b').catch(err => {
-    t.strictEqual(
-      err.toString(),
-      'TypeError: Expected 1 or 2 arguments (path: String[, options: Object]), but got 3 arguments.',
-      'should fail when it takes too many arguments.'
-    );
-  });
+	try {
+		await listEmptyFiles('a', {}, 'b');
+		fail();
+	} catch (err) {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected 1 or 2 arguments (path: <string|Buffer|URL>[, options: <Object>]), but got 3 arguments.',
+			'should fail when it takes too many arguments.'
+		);
+	}
+
+	t.end();
 });
